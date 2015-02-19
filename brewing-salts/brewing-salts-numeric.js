bf3fedbcc5f4791f31cba808d0b238f9fc81b618
Jim Cipar <jcipar@fiksu.com>
2015-02-19 15:39:47 +0000

    lots of improvements

--- brewing-salts/brewing-salts-numeric.js
+++ brewing-salts/brewing-salts-numeric.js
@@ -165,6 +165,68 @@ var example_output = {
 };
 
 
+var standard_initial_profiles = {
+    "Distilled": {
+        "calcium": 0,
+        "magnesium": 0,
+        "sodium": 0,
+        "sulfate": 0,
+        "chloride": 0,
+        "bicarbonate": 0
+    },
+    "Boston, Dec. 2014":  {
+        "calcium": 4.0,
+        "magnesium": 0.9,
+        "sodium": 32.6,
+        "sulfate": 5.5,
+        "chloride": 23.2,
+        "bicarbonate": 49.2
+    },
+    "Cambridge, MA, 2013": {
+        "calcium": 20.8,
+        "magnesium": 4.0,
+        "sodium": 79.0,
+        "sulfate": 28.0,
+        "chloride": 120.0,
+        "bicarbonate": 60.5
+    },
+    "Ocean": {
+        "calcium": 412,
+        "magnesium": 1284,
+        "sodium": 10781,
+        "sulfate": 2712,
+        "chloride": 19353,
+        "bicarbonate": 126
+    }
+}
+
+var standard_target_profiles = {
+    "Yellow Malty": {
+        "calcium": 50.0,
+        "magnesium": 5.0,
+        "sodium": 5.0,
+        "sulfate": 55.0,
+        "chloride": 70.0,
+        "bicarbonate": 0.0
+    },
+    "Amber Balanced": {
+        "calcium": 55.0,
+        "magnesium": 10.0,
+        "sodium": 10.0,
+        "sulfate": 75.0,
+        "chloride": 63.0,
+        "bicarbonate": 40.0
+    },
+    "Ocean": {
+        "calcium": 412,
+        "magnesium": 1284,
+        "sodium": 10781,
+        "sulfate": 2712,
+        "chloride": 19353,
+        "bicarbonate": 126
+    }
+}
+
 
 function setup_problem(parameters, input)
 {
@@ -268,25 +330,6 @@ function ion_salt_map(parameters)
     }
     return im;
 }
-
-
-var problem = {
-    "objective": {
-        "abse_calcium": 1.0,
-        "abse_sulfate": 1.0
-    },
-    "constraints": [
-        {
-            "rhs": 0.0,
-            "lhs": {"e_calcium": 1.0, "abse_calcium": -1.0}
-        },
-        {
-            "rhs": 0.0,
-            "lhs": {"e_calcium": -1.0, "abse_calcium": -1.0}
-        }
-    ]
-}
-
 
 
 function problem_to_matrix(problem)
